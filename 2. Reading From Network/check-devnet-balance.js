4dab822453df886ef1469bfbea599cf272ff7416
Raj Karmakar <97rajmath@gmail.com>
2024-10-08 20:30:38 +0530

    Check validity of Solana Address

--- 2. Reading From Network/check-devnet-balance.js
+++ 2. Reading From Network/check-devnet-balance.js
@@ -5,6 +5,10 @@ const publicKey = new PublicKey("5tvgTybF31BWqZyjed9Qz6B4hcA92Vc7zALuhyv24W76");
 const connection = new Connection("https://api.devnet.solana.com", "confirmed");
 
 const getBalance = async () => {
+	if (!PublicKey.isOnCurve(publicKey.toBuffer())) {
+		console.log("Invalid address provided");
+		return
+	}
 	return connection.getBalance(publicKey)
 		.then(balance => {
 			console.log(`The balance of the account is ${balance} lamports`);
