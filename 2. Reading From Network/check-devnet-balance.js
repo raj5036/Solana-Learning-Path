27f1c274bcb5b7c646de6aaa918d371689e1bc63
Raj Karmakar <97rajmath@gmail.com>
2024-10-08 20:52:51 +0530

    Check MAINNET token balances of famous accounts

--- 2. Reading From Network/check-devnet-balance.js
+++ 2. Reading From Network/check-devnet-balance.js
@@ -1,18 +1,27 @@
 const { Connection, clusterApiUrl, PublicKey, LAMPORTS_PER_SOL } = require("@solana/web3.js")
 
-const publicKey = new PublicKey("5tvgTybF31BWqZyjed9Qz6B4hcA92Vc7zALuhyv24W76");
 
-const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+const MAINNET_BETA_ENDPOINT="https://api.mainnet-beta.solana.com";
+const DEVNET_ENDPOINT="https://api.devnet.solana.com";
 
-const getBalance = async () => {
+const MAINNET_ACCOUNTS = [
+	{domain: "toly.sol", address: "86xCnPeV69n6t3DnyGvkKobf9FdN2H9oiVDdaMpo2MMY"},
+	{domain: "shaq.sol", address: "gacMrsrxNisAhCfgsUAVbwmTC3w9nJB6NychLAnTQFv"},
+	{domain: "mccann.sol", address: "JCZjJcmuWidrj5DwuJBxwqHx7zRfiBAp6nCLq3zYmBxd"},
+]
+
+const connection = new Connection(MAINNET_BETA_ENDPOINT, "confirmed");
+
+const getBalance = async (address) => {
+	const publicKey = new PublicKey(address.address);
+
 	if (!PublicKey.isOnCurve(publicKey.toBuffer())) {
 		console.log("Invalid address provided");
 		return
 	}
 	return connection.getBalance(publicKey)
 		.then(balance => {
-			console.log(`The balance of the account is ${balance} lamports`);
-			console.log(`The balance of the account is ${balance / LAMPORTS_PER_SOL} SOL`);
+			console.log(`The balance of the account ${address.domain} is ${balance} lamports or ${balance / LAMPORTS_PER_SOL} SOL`);
 			return balance
 		})
 		.catch(err => {
@@ -20,4 +29,4 @@ const getBalance = async () => {
 		});
 }
 
-getBalance()
+MAINNET_ACCOUNTS.forEach(getBalance)
